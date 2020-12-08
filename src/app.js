944ab1fe4de5d4810d642e297c5c8616395536f4
anna-mars <73495328+anna-mars@users.noreply.github.com>
2020-12-08 22:58:33 +0100

    added 1st part of the java script

--- src/app.js
+++ src/app.js
@@ -1,1 +1,4 @@
-
+let apiKey = "58a0b6e26263101abdfda8d9e9d3a0f0";
+let city = "Amsterdam";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
