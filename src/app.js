1b5067dc0ee7e1821c9d149077f99e675dce5047
anna-mars <73495328+anna-mars@users.noreply.github.com>
2020-12-09 15:50:21 +0100

    Added the js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,43 @@
+function formatDate(presentTime) {
+  let date = new Date(presentTime);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+function displayTemperature(response) {
+  console.log(response);
+  let temperatureElement = document.querySelector("#temperature");
+  let temperatureMaxElement = document.querySelector("#max-temp");
+  let temperatureMinElement = document.querySelector("#min-temp");
+  let cityElement = document.querySelector("#city");
+  let countryElement = document.querySelector("#country");
+  let skyElement = document.querySelector("#sky");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let pressureElement = document.querySelector("#pressure");
+  let dateElement = document.querySelector("#date");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  temperatureMaxElement.innerHTML = Math.round(response.data.main.temp_max);
+  temperatureMinElement.innerHTML = Math.round(response.data.main.temp_min);
+  cityElement.innerHTML = response.data.name;
+  countryElement.innerHTML = response.data.sys.country;
+  skyElement.innerHTML = response.data.weather[0].main;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  pressureElement.innerHTML = response.data.main.pressure;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+}
 let apiKey = "58a0b6e26263101abdfda8d9e9d3a0f0";
-let city = "Amsterdam";
+let city = "amsterdam";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
