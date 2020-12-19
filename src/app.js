3757067dd100eb00f3ddce655b2cb829afad606c
anna-mars <73495328+anna-mars@users.noreply.github.com>
2020-12-19 14:34:45 +0100

    Added forecast-bottom the java script
    function to bottom part of forecast

--- src/app.js
+++ src/app.js
@@ -24,8 +24,19 @@ function formatDate() {
   document.querySelector("#time").innerHTML = `${date}.${month}.${year}`;
 }
 formatDate();
+
+function formatDay(timestamp) {
+  let dateTime = new Date(timestamp);
+
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let day = days[dateTime.getDay()];
+  day = day.slice(0, 3);
+  return `${day}`;
+}
 function displayTemperature(response) {
-  console.log(response);
+  let lon = response.data.coord.lon;
+  let lat = response.data.coord.lat;
+  let apiKey = "58a0b6e26263101abdfda8d9e9d3a0f0";
   let temperatureElement = document.querySelector("#temperature");
   let temperatureMaxElement = document.querySelector("#max-temp");
   let temperatureMinElement = document.querySelector("#min-temp");
@@ -60,6 +71,35 @@ function displayTemperature(response) {
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   mainIconElement.setAttribute("alt", response.data.weather[0].description);
+
+  forecastApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  axios.get(forecastApiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+  console.log(response);
+  for (let index = 1; index < 7; index++) {
+    forecast = response.data.daily[index];
+
+    forecastElement.innerHTML += `
+  <div class=col-2>
+            <h5>
+             ${formatDay(forecast.dt * 1000)}
+            </h5>
+            <img src="https://openweathermap.org/img/wn/${
+              forecast.weather[0].icon
+            }@2x.png" id="forecast-icon">
+            <div class="forecast-bottom-temp">
+              <strong class="main-max">${Math.round(
+                forecast.temp.max
+              )}</strong>° |
+              <span class="main-min">${Math.round(forecast.temp.min)}</span>°
+            </div>
+          </div>`;
+  }
 }
 
 function search(city) {
